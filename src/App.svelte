e58fc81cf089faa86a2fc8448c5563155d0f08b7
mhdtarek <mhdtarekalkasemi@gmail.com>
2022-08-26 15:44:49 +0200

    starter

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Router from 'svelte-spa-router'
+  import Game from './routes/Game.svelte';
+  import Home from './routes/Home.svelte'
+  import NotFound from './routes/NotFound.svelte';
+  let routes = {
+    "/": Home,
+    "/Game:id": Game,
+    "*": NotFound
+  }
+  console.log(import.meta.env.VITE_FIREBASE_CONFIG)
+</script>
+
+<main>
+<Router {routes}/>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+</style>
