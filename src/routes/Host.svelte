4902e4939b29ce8948235571bc5df8a1833ad014
mhdtarek <mhdtarekalkasemi@gmail.com>
2022-08-28 14:35:53 +0200

    :)

--- src/routes/Host.svelte
+++ src/routes/Host.svelte
@@ -3,6 +3,8 @@ import { collection, addDoc } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { MaterialApp, Button} from 'svelte-materialify';
+import { doc, onSnapshot } from "firebase/firestore";
+import QrCode from "svelte-qrcode"
 
 const firebaseConfig = {
     apiKey: "AIzaSyD4Aq2zW_aVeeiWv1Argn3OhuGcjH0Heps",
@@ -13,26 +15,45 @@ const firebaseConfig = {
     appId: "1:353241258367:web:ddd6d966a5d0acaecb224a",
     measurementId: "G-BRWMTFYP0D"
   };
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  let docRefVar = ""
-  var test = "yes"
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+let game = {}
+let gameCreated = false
+let gameId = ""
+let webUrl = "https://  "
+
 // Add a new document with a generated id.
 async function generateGame() {
 const docRef = await addDoc(collection(db, "games"), {
     gameState: "Starting",
     type: "default",
-    test
+    playerCount: 0,
+    players: {},
+    scoreMode: "default"
 });
-console.log("Document written with ID: ", docRef.id);
+console.log("created")
+readGame(docRef.id)
+gameCreated = true
+gameId = docRef.id
 }
 
+function readGame(docReff) {
+    
+    const unsub = onSnapshot(doc(db, "games", docReff), (doc) => {
+        game = doc.data()
+        console.log("Current data: ", doc.data());
+
+
+});
+}
 </script>
 
 
 <main>
-
-hi
-<Button on:click={generateGame}></Button>
-    
+{#if !gameCreated}
+<Button on:click={generateGame}>skapa spel</Button>
+ {/if}   
+ {#if gameCreated}
+his
+ {/if}
 </main>
