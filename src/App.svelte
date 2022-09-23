3b83837f72828d46681bae97780f126492061e17
mhdtarek <mhdtarekalkasemi@gmail.com>
2022-09-23 14:03:55 +0200

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,46 @@
 <script lang="ts">
-  import Router from 'svelte-spa-router'
-  import Game from './routes/Game.svelte';
-  import Home from './routes/Home.svelte'
-  import NotFound from './routes/NotFound.svelte';
-  import Host from './routes/Host.svelte';
-  import { AppBar, Button, Icon, Menu, ListItem, MaterialApp } from 'svelte-materialify';
-    let routes = {
+  import Router from "svelte-spa-router";
+  import Game from "./routes/Game.svelte";
+  import Home from "./routes/Home.svelte";
+  import NotFound from "./routes/NotFound.svelte";
+  import Host from "./routes/Host.svelte";
+  import {
+    AppBar,
+    Button,
+    Icon,
+    Menu,
+    ListItem,
+    MaterialApp,
+  } from "svelte-materialify";
+  let routes = {
     "/": Home,
     "/Game/:id": Game,
     "/HostGame": Host,
-    "*": NotFound
-    }
+    "*": NotFound,
+  };
 </script>
 
 <main>
-<MaterialApp>
-<AppBar>
-  <span slot="title"><a href="/#/">multimath</a></span>
-  <div style="flex-grow:1" />
-  <Button><a href="/#/HostGame">Skapa Spel</a></Button>
-</AppBar>
-</MaterialApp>
-<Router {routes}/>
+  <MaterialApp>
+    <AppBar>
+      <span slot="title"><a href="/#/">multimath</a></span>
+      <div style="flex-grow:1" />
+      <Button><a href="/#/HostGame">Skapa Spel</a></Button>
+    </AppBar>
+  </MaterialApp>
+  <Router {routes} />
 </main>
 
 <style>
   :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   }
-  a, a:hover, a:focus, a:active {
-      text-decoration: none;
-      color: inherit;
- }
+  a,
+  a:hover,
+  a:focus,
+  a:active {
+    text-decoration: none;
+    color: inherit;
+  }
 </style>
--- src/routes/Host.svelte
+++ src/routes/Host.svelte
@@ -1,213 +1,69 @@
-<script lang="ts">
-  import { collection, addDoc } from "firebase/firestore";
-  import { initializeApp } from "firebase/app";
-  import { getFirestore } from "firebase/firestore";
+<script>
   import {
-    MaterialApp,
-    Button,
-    Chip,
-    Icon,
     Container,
     Row,
     Col,
-    TextField,
-    Select,
+    Card,
+    CardTitle,
+    CardSubtitle,
+    CardActions,
+    Button,
+    Icon,
+    Divider,
+    MaterialApp,
   } from "svelte-materialify";
-  import { doc, onSnapshot, updateDoc } from "firebase/firestore";
-  import { mdiAccount } from "@mdi/js";
-  import QRCode from "../lib/QRJS.svelte";
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyD4Aq2zW_aVeeiWv1Argn3OhuGcjH0Heps",
-    authDomain: "multimath-9eb3f.firebaseapp.com",
-    projectId: "multimath-9eb3f",
-    storageBucket: "multimath-9eb3f.appspot.com",
-    messagingSenderId: "353241258367",
-    appId: "1:353241258367:web:ddd6d966a5d0acaecb224a",
-    measurementId: "G-BRWMTFYP0D",
-  };
-  const app = initializeApp(firebaseConfig);
-  const db = getFirestore(app);
-  let game = {};
-  let players = [];
-  let gameCreated = false;
-  let gameId = "";
-  let uri = "";
-  let playersCount = 0;
-  let currentGameState = "";
-  let maxScore = "";
-  let maxScoreNumber = 100;
-  let playerscheck = [];
-  let value = [];
-  const items = [
-    { name: "+, -", value: ["+", "-"] },
-    { name: "*, /", value: ["*", "-"] },
-    { name: "*, /, +, -", value: ["*", "-", "+", "/"] },
-    { name: "*, +", value: ["*", "+"] },
-    { name: "/, -", value: ["/", "-"] },
-  ];
-  const rules = [(v) => v.length <= 20 || "Max 20 characters"];
-
-  // Add a new game with a generated id.
-  async function generateGame() {
-    const equations = value;
-    const docRef = await addDoc(collection(db, "games"), {
-      gameState: "Starting",
-      type: "default",
-      playerCount: 0,
-      players: [],
-      scoreMode: "default",
-      equations,
-    });
-    readGame(docRef.id);
-    gameCreated = true;
-    gameId = docRef.id;
-    uri = "https://multimath.vercel.app/#/Game/" + gameId;
-    maxScoreNumber = Number(maxScore);
-    console.log(maxScoreNumber);
-  }
-
-  async function StartGame(gameRef) {
-    const washingtonRef = doc(db, "games", gameId);
-    await updateDoc(washingtonRef, {
-      gameState: "started",
-    });
-  }
-
-  // read the game the host has created
-  function readGame(gameRef) {
-    const unsub = onSnapshot(doc(db, "games", gameRef), (doc) => {
-      game = doc.data();
-      console.log("Current data: ", doc.data());
-      players = doc.data().players;
-      playersCount = players.length;
-      currentGameState = doc.data().gameState;
-
-      // Sort the players.
-      players.sort(function (a, b) {
-        return b.score - a.score;
-      });
-
-      //check if any user has the end score
-      playerscheck = players.filter((player) => {
-        return player.score === maxScoreNumber;
-      });
-
-      //if there is, end the game.
-      if (playerscheck.length >= 1) {
-        endGame();
-      }
-    });
-  }
-
-  async function endGame() {
-    const washingtonRef = doc(db, "games", gameId);
-    await updateDoc(washingtonRef, {
-      gameState: "ended",
-    });
-  }
+  import { slide } from "svelte/transition";
+  import { mdiChevronDown } from "@mdi/js";
 </script>
 
 <main>
   <MaterialApp>
-    {#if !gameCreated}
-      <Row>
-        <Col />
-        <Col>
-          <TextField clearable counter={20} bind:value={maxScore} {rules}>
-            Poäng för att sluta spelet
-          </TextField>
-          <Select {items} bind:value>Typ av spel</Select>
-          <Button on:click={generateGame}>skapa spel</Button>
-        </Col>
-        <Col />
-      </Row>
-    {/if}
-
     <Container>
-      <!-- check if game created-->
-      {#if gameCreated}
-        <!-- check if game is starting-->
-        {#if currentGameState === "Starting"}
-          <div class="right">
-            <Button size="small" on:click={StartGame}>starta spel</Button>
-          </div>
-          <div class="center ">
-            <QRCode codeValue={uri} squareSize="150" />
-          </div>
-          {#each players as player}
-            <Chip class="ma-2 blue darken-4 blue-text darken-1">
-              <Icon path={mdiAccount} />
-              <span>{player.name}</span>
-            </Chip>
-          {/each}
-        {/if}
-        <!-- check if game has started-->
-        {#if currentGameState === "started"}
-          <div
-            class="elevation-2 yellow"
-            style="padding: 30px; margin-bottom: 30px;"
-          >
-            <Row noGutters>
-              <Col cols={4} offset={6} offset_md={4}>
-                <div class="pa-2 text-center">
-                  <h5>flest poäng</h5>
-                  <p>{players[0].name}</p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          <div class="elevation-2">
-            <Row noGutters>
-              <Col cols={4} offset={6} offset_md={4}>
-                <div class="pa-2 text-center">
-                  {#each players as p}
-                    <p>{p.name} | {p.score}</p>
-                  {/each}
-                </div>
-              </Col>
-            </Row>
-          </div>
-        {/if}
-        {#if currentGameState === "ended"}
-          <div
-            class="elevation-2 yellow"
-            style="padding: 30px; margin-bottom: 15px;"
-          >
-            <Row noGutters>
-              <Col cols={4} offset={6} offset_md={4}>
-                <div class="pa-2 text-center">
-                  <h4>VINNARE</h4>
-                  <p>{players[0].name}</p>
-                </div>
-              </Col>
-            </Row>
-          </div>
-          <div class="elevation-2">
-            <Row noGutters>
-              <Col cols={4} offset={6} offset_md={4}>
-                <div class="pa-2 text-center">
-                  {#each players as p}
-                    <p>{p.name} | {p.score}</p>
-                  {/each}
-                </div>
-              </Col>
-            </Row>
-          </div>
-        {/if}
-      {/if}
+      <Row>
+        <Col>
+          <Card style="max-width:350px;">
+            <img src="" alt="background" />
+            <CardTitle>Top western road trips</CardTitle>
+            <CardSubtitle>1,000 miles of wonder</CardSubtitle>
+            <CardActions>
+              <Button text>Button</Button>
+            </CardActions>
+          </Card>
+        </Col>
+        <Col>
+          <Card style="max-width:350px;">
+            <img src="//picsum.photos/350" alt="background" />
+            <CardTitle>Top western road trips</CardTitle>
+            <CardSubtitle>1,000 miles of wonder</CardSubtitle>
+            <CardActions>
+              <Button text>Button</Button>
+            </CardActions>
+          </Card>
+        </Col>
+        <Col>
+          <Card style="max-width:350px;">
+            <img src="//picsum.photos/350" alt="background" />
+            <CardTitle>Top western road trips</CardTitle>
+            <CardSubtitle>1,000 miles of wonder</CardSubtitle>
+            <CardActions>
+              <Button text>Button</Button>
+            </CardActions>
+          </Card>
+        </Col>
+        <Col>
+          <Card style="max-width:350px;">
+            <img src="//picsum.photos/350" alt="background" />
+            <CardTitle>Top western road trips</CardTitle>
+            <CardSubtitle>1,000 miles of wonder</CardSubtitle>
+            <CardActions>
+              <Button text>Button</Button>
+            </CardActions>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   </MaterialApp>
 </main>
 
 <style>
-  .center {
-    display: flex;
-    justify-content: center;
-  }
-  .right {
-    display: flex;
-    justify-content: right;
-    margin-bottom: 0;
-  }
 </style>
